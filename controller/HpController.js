e75c47a236914ad65240cd311706a37d8c4743a8
Danica <danicacolobong1@gmail.com>
2024-09-26 22:52:33 +0800

    first commit

---
+++ controller/HpController.js
@@ -0,0 +1,57 @@
+const con = {
+    index:(req, res)=>{
+        res.render('index');   
+    },
+    chappell:(req, res)=>{
+        res.render('chappell');   
+    },
+    niki:(req, res)=>{
+        res.render('niki');   
+    },
+    taylor:(req, res)=>{
+        res.render('taylor');   
+    },
+    laufey:(req, res)=>{
+        res.render('laufey');   
+    },
+    harry:(req, res)=>{
+        res.render('harry');   
+    },
+    bea:(req, res)=>{
+        res.render('bea');   
+    },
+    tomorrow:(req, res)=>{
+        res.render('tomorrow');   
+    },
+    w2e:(req, res)=>{
+        res.render('w2e');   
+    },
+    rise:(req, res)=>{
+        res.render('rise');   
+    },
+    ttpd:(req, res)=>{
+        res.render('ttpd');   
+    },
+    aug:(req, res)=>{
+        res.render('aug');   
+    },
+    sobs:(req, res)=>{
+        res.render('sobs');   
+    },
+    rnb:(req, res)=>{
+        res.render('rnb');   
+    },
+    ghibli:(req, res)=>{
+        res.render('ghibli');   
+    },
+    supercut:(req, res)=>{
+        res.render('supercut');   
+    },
+    waiting:(req, res)=>{
+        res.render('waiting');   
+    },
+    list:(req, res)=>{
+        res.render('list');   
+    },
+};
+module.exports = con;
